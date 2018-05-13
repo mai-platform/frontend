548f675bfabe0e36f2d3aba8e23deae7c30949dd
oberdev <qovalski@gmail.com>
2018-05-13 04:31:15 +0300

    complited metro autocomplete

--- app/js/common.js
+++ app/js/common.js
@@ -123,5 +123,49 @@ autoComplete.controller('CompleteCity', ['$scope','$http','$tether', function($s
 }])
 autoComplete.controller('CompleteSubway', ['$scope','$http','$tether', function($scope, $http,$tether){
   $scope.tether = $tether;
-  console.log($tether);
+  var $subway = $('#subway');
+  $subway.autocomplete({
+    source: function (request, responce) {
+        var query = request.term;
+        $.ajax({
+          url:"https://api.hh.ru/metro/1",
+          dataType: "json",
+          success: function(data, textStatus, request) {
+            var stations = new Array();
+            data.lines.forEach(function(line, i){
+              line.stations.forEach(function(station, j){
+                stations.push({
+                    name: station.name,
+                    color: line.hex_color
+                });
+              });
+            });
+            stations = stations.filter(function(station) {
+              return station.name.toLowerCase().indexOf(query.toLowerCase()) != -1;
+            });
+            stations = stations.sort(function (a, b) {
+              if (a.name.toLowerCase().indexOf(query.toLowerCase()) > b.name.toLowerCase().indexOf(query.toLowerCase()))
+                return 1;
+              if (a.name.toLowerCase().indexOf(query.toLowerCase()) < b.name.toLowerCase().indexOf(query.toLowerCase()))
+                return -1;
+              return 0;
+            });
+            responce(stations.slice(0,4));
+          }
+        });
+    },
+    select: function( event, ui ) {
+        console.log(ui.item.name);
+        $('#subway').val(ui.item.name);
+        return false;
+    },
+    focus: function( event, ui ) {
+        return false;
+    },
+    autoFocus: true
+  }).autocomplete( "instance" )._renderItem = function (ul, station) {
+      return $( "<li class='ui-menu-item'>" )
+        .append( "<div class='ui-menu-item-wrapper'>" + "<i class='fas fa-circle' style='color:#" + station.color +"'></i>" + " " + station.name + "</div>")
+        .appendTo( ul );
+  }
 }])
